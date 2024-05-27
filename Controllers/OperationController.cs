8073aa47e504ada13373a21ed8b3fa5689862373
Antonio Espinoza <anespinoza@baufest.com>
2024-05-27 12:24:01 -0400

    configuration for controllers ready

--- Controllers/OperationController.cs
+++ Controllers/OperationController.cs
@@ -9,9 +9,34 @@ namespace backend.Controllers
     public class OperationController : ControllerBase
     {
         [HttpGet]
-        public decimal Add(decimal a, decimal b)
+        public decimal Get(decimal a, decimal b)
         {
             return a + b;
         }
+        [HttpPost]
+        public decimal Add(Numbers numbers, [FromHeader] string Host,
+            [FromHeader(Name = "Content-Length")] string ContentLengh) //Obtener el valor de cualquier header
+        {
+            Console.WriteLine(Host);
+            Console.WriteLine(ContentLengh);
+            return numbers.A - numbers.B;
+        }
+        [HttpPut]
+        public decimal Edit(decimal a, decimal b)
+        {
+            return a * b;
+        }
+        [HttpDelete]
+        public decimal Delete(decimal a, decimal b)
+        {
+            return a / b;
+        }
     }
+
+    public class Numbers
+    {
+        public decimal A {get; set;}
+        public decimal B {get; set;}
+    }
+
 }
--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3bcf96d7df4f5360fd567d3e99a59eadd091ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
