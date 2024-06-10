a4ff966b3a4a5b4c2e3554c163c9a2dda7e31d1f
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-06-10 18:02:19 -0400

    add IActionResult

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8d62e408836d46c976fc02544bbfe5daa52bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3777c96d5e2b0777768f033c08ecaa53b1d03b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
